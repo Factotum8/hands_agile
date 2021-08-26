cfd96b7cddba43048fa43ef5c7f50b7dce8ea15d
Alexandr Amochkin <a.amochkin@latera.ru>
2021-08-26 11:28:44 +0300

    Refactoring blank symbol

--- game/points_calculator.py
+++ game/points_calculator.py
@@ -6,19 +6,19 @@ class PointsCalculator:
     The class calculates win points for players.
     """
 
+    blank_symbol = '.'
+
     def __init__(self, finger_count: int, game_field: str):
         self.finger_count = finger_count * 2
         game_field = self.prepare_field(game_field)
         self.game_field = game_field
         self.appear_frequency = Counter(game_field)
-        del self.appear_frequency['.']
 
-    @staticmethod
-    def prepare_field(game_field: str):
+    def prepare_field(self, game_field: str):
         """
         Remove dots from the game field & expand the game field to one dimension list.
         """
-        return [int(i) for line in game_field for i in line if i != '.']
+        return [int(i) for line in game_field for i in line if i != self.blank_symbol]
 
     def calculate(self) -> int:
         """
